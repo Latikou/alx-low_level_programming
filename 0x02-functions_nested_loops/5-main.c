9636ceea68997992eee19c71be122d1653f70912
Latikou <latifakouhaili469@gmail.com>
2023-04-25 16:56:14 -0700

    task,3,4,5,6,7,8,9,10,11,12,13,14,15,16

--- 0x02-functions_nested_loops/5-main.c
+++ 0x02-functions_nested_loops/5-main.c
@@ -1,13 +1,14 @@
+#include <limits.h>
 #include "main.h"
 
 /**
- * main - check the code.
+ * main - check the code for Holberton School students.
  *
  * Return: Always 0.
  */
 int main(void)
 {
-    int r;
+	int r;
 
 	r = print_sign(98);
 	if (r < 0)
