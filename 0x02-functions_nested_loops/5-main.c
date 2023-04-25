f9232b62dfc3c9dffe052576dc76d0b43ddcdfff
Latikou <latifakouhaili469@gmail.com>
2023-04-25 16:46:58 -0700

    task,3,4,5,6,7,8,9,10,11,12,13,14,15,16

--- 0x02-functions_nested_loops/5-main.c
+++ 0x02-functions_nested_loops/5-main.c
@@ -9,8 +9,10 @@ int main(void)
 {
     int r;
 
-	r = print_last_digit(98);
-	_putchar('0' + r);
+	r = print_sign(98);
+	if (r < 0)
+		r *= -1;
+	_putchar(r + '0');
 	_putchar('\n');
 	return (0);
 }
