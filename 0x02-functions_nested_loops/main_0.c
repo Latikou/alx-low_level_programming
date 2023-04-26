340eb07fd912a25b7f12fcbd3379070b1128733b
Latikou <latifakouhaili469@gmail.com>
2023-04-25 17:12:47 -0700

    task 5

--- 0x02-functions_nested_loops/main_0.c
+++ 0x02-functions_nested_loops/main_0.c
@@ -8,12 +8,16 @@
  */
 int main(void)
 {
-	int r;
+        int r;
 
-	r = print_sign(98);
-	if (r < 0)
-		r *= -1;
-	_putchar(r + '0');
-	_putchar('\n');
-	return (0);
+        r = print_sign(98);
+        if (r > 0)
+                _putchar('+');
+        else if (r == 0)
+                _putchar('0');
+        else
+                _putchar('-');
+        _putchar('\n');
+        return (0);
 }
+
