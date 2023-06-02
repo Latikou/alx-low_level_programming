ae879dab3d8586bdcdf93aff41244e5a58927339
latikou <latifakouhaili469@gmail.com>
2023-06-02 00:43:02 -0700

    tasks done

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -2,15 +2,15 @@
 #include "lists.h"
 
 /**
- * print_list - prints a linked lists
- * @h: pointer to first node
+ * print_list - prints all the elements of linked list
+ * @h: pointer to the list_t list to print
  *
- * Return: size of list
+ * Return: the number of nodes printed
  */
 size_t print_list(const list_t *h)
 {
        size_t s = 0;
-	
+
        while (h)
        {
              if (!h->str)
