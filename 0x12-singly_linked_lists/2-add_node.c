4e294665e98ead03eb0308690821bdf4e1b22fde
latikou <latifakouhaili469@gmail.com>
2023-06-02 09:58:18 -0700

    task 2

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -12,7 +12,7 @@ list_t *add_node(list_t **head, const char *str)
 	list_t *new_head = malloc(sizeof(list_t));
 
 	if (!head || !new_head)
-		return (Null);
+		return (NULL);
 	if (str)
 	{
 		new_head->str = strdup(str);
