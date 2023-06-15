dca6944b7224db7231c70845c76270d436469c1e
latikou <latifakouhaili469@gmail.com>
2023-06-15 08:24:14 -0700

    tasks done

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -12,7 +12,7 @@ int create_file(const char *filename, char *text_content)
 {
 	int fd, w, len = 0;
 
-	if(filename == NULL)
+	if (filename == NULL)
 		return (-1);
 
 	if (text_content != NULL)
@@ -21,7 +21,7 @@ int create_file(const char *filename, char *text_content)
 			len++;
 	}
 
-	fd = oppen(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 	w = write(fd, text_content, len);
 
 	if (fd == -1 || w == -1)
